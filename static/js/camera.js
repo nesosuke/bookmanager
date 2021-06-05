8ff9b26b438e1ada2357eebb7524508354c6aa28
nesosuke <neso@neso.tech>
2021-06-05 11:30:34 +0900

    camera test

---
+++ static/js/camera.js
@@ -0,0 +1,20 @@
+const medias = {
+    audio: false, video: {
+        facingMode: {
+            exact: "environment"
+        }
+    }
+},
+const video = document.getElementById("video");
+const promise = navigator.mediaDevices.getUserMedia(medias);
+
+promise.then(successCallback)
+    .then(errorCallback);
+
+function successCallback(stream) {
+    video.srcObject = stream;
+};
+
+function errorCallback(err) {
+    alert(err);
+};
